a5265a78d004870f10c839c4e0f00a3996b3976a
fengkan <fengkan@sensetime.com>
2020-03-10 13:57:48 +0800

    Update index-https.js.

--- index-https.js
+++ index-https.js
@@ -1,25 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const fs = require('fs-extra')
 const bodyParser = require('body-parser')
-var path = require('path')
+const path = require('path')
+const sep = path.sep;
+const http = require('http')
+const https = require('https')
 
-var http = require('http')
-var https = require('https')
+const privateKey = fs.readFileSync(path.join(__dirname, './certificate/private.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, './certificate/file.crt'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
-var privateKey  = fs.readFileSync(path.join(__dirname, './certificate/private.pem'), 'utf8');
-var certificate = fs.readFileSync(path.join(__dirname, './certificate/file.crt'), 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-var PORT = 3001;
-var SSLPORT = 3002;
+const PORT = 3001;
+const SSLPORT = 3002;
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json()
 
 // create application/x-www-form-urlencoded parser
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -30,7 +29,7 @@ fs.mkdirSync('./log')
 //app.get('/', (req, res) => res.send('Dash push demo has been started.'))
 
 app.get('/', function (req, res) {
-    if(req.protocol === 'https') {
+    if (req.protocol === 'https') {
         res.status(200).send('This is https visit!');
     }
     else {
@@ -47,21 +46,53 @@ function guid() {
 }
 
 app.post('/', jsonParser, (req, res) => {
-    var i = 0
+    var eventTypeMap = {
+        "1": "白名单比中",
+        "2": "白名单异常",
+        "4": "陌生人",
+        "3": "黑名单告警",
+        "5": "非活体攻击",
+        "6": "密码攻击",
+        "101": "人体抓拍记录",
+        "102": "人体越线告警",
+        "103": "人体区域闯入告警",
+        "104": "车辆违停告警",
+        "105": "车辆抓拍记录",
+        "106": "非机动车抓拍记录",
+    }
+    var typeMap = {
+        "1": "人脸",
+        "2": "结构化"
+    }
+
+    var count = 0
     var body = req.body
-    var taskId = body.taskInfo.task.taskId
-    var trackId = body.detectInfo.faceInfo.trackId
-    var objectId = body.objectId
-    var capturedTime = body.detectInfo.capturedTime
-    var receivedTime = body.detectInfo.receivedTime
-    var quality = body.detectInfo.faceInfo.quality
-    var score = body.detectInfo.score
+    var eventType = body.eventType
+    var type = body.type
+    var deviceId = body.data.deviceInfo.device.deviceId
+    if (body.data.hasOwnProperty('taskId')) {
+        var taskId = body.data.taskInfo.task.taskId
+    }
+    var logPath = './log' + sep + typeMap[type] + sep + eventTypeMap[eventType] + sep + deviceId + sep + taskId
+    try {
+        fs.mkdirSync(logPath, { recursive: true });
+    } catch (e) {
+        console.log('Cannot create folder ', e);
+    }
+    console.log("type:" + typeMap[type] + " eventType:" + eventTypeMap[eventType] + " deviceId: " + deviceId + " taskId:" + taskId)
+    // var taskId = body.taskInfo.task.taskId
+    // var trackId = body.detectInfo.faceInfo.trackId
+    // var objectId = body.objectId
+    // var capturedTime = body.detectInfo.capturedTime
+    // var receivedTime = body.detectInfo.receivedTime
+    // var quality = body.detectInfo.faceInfo.quality
+    // var score = body.detectInfo.score
     var uuid = guid()
-    var fileName = taskId + "|" + trackId + "|" + objectId + "|" + capturedTime + "|" + receivedTime + "|" + quality  + "|" + score + "|" + uuid
-    console.log(fileName)
-    var i = i + 1
+    // var fileName = taskId + "|" + trackId + "|" + objectId + "|" + capturedTime + "|" + receivedTime + "|" + quality + "|" + score + "|" + uuid
+    var fileName = uuid
+    count += 1
     // With a callback:
-    fs.writeJson('./log/' + fileName + '.json', body, err => {
+    fs.writeJson(logPath + sep + fileName + '.json', body, err => {
         if (err)
             return console.error(err)
         res.send('Dash push demo get post request.')
@@ -71,12 +102,12 @@ app.post('/', jsonParser, (req, res) => {
 //httpServer.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 //创建http服务器
-httpServer.listen(PORT, function() {
+httpServer.listen(PORT, function () {
     console.log('HTTP Server is running on: http://localhost:%s', PORT);
 });
- 
+
 //创建https服务器
-httpsServer.listen(SSLPORT, function() {
+httpsServer.listen(SSLPORT, function () {
     console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
 });
 
